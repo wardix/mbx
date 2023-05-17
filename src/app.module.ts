ee577e025f3c74f877764adc55d73f4528585c87
Wardi <wardi@nusa.net.id>
2023-05-17 10:20:59 +0700

    revert commit 0a25e07674068d6b6c299a6bcade0a605b930b85
    revert commit remove entities from app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { Phonebook } from './customers/entities/phonebook.entity';
 import { FeedbackModule } from './feedback/feedback.module';
+import { NocIssue } from './noc/entities/noc-issue.entity';
 import { NocModule } from './noc/noc.module';
+import { SmsInbox } from './sms/entities/sms-inbox.entity';
+import { SmsSatisfaction } from './sms/entities/sms-satisfaction.entity';
+import { SmsSentSatisfaction } from './sms/entities/sms-sent-satisfaction.entity';
+import { SmsSent } from './sms/entities/sms-sent.entity';
 import { SmsModule } from './sms/sms.module';
 
 @Module({
@@ -18,7 +24,14 @@ import { SmsModule } from './sms/sms.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DBNAME,
-      entities: [],
+      entities: [
+        SmsInbox,
+        SmsSent,
+        SmsSentSatisfaction,
+        SmsSatisfaction,
+        NocIssue,
+        Phonebook,
+      ],
       synchronize: false,
     }),
     FeedbackModule,
